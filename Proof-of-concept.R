34d33550056b092bdc058ac4581fead7110654bf
raulmejia <r8a8m8p8@gmail.com>
2021-02-18 21:31:19 +0100

    makeallContrasts 1.0

--- Proof-of-concept.R
+++ Proof-of-concept.R
@@ -5,7 +5,6 @@ library("tidyverse")
 # the name of your groups could be the non duplicated elements of
 # annotations dataframe
 
-
 sd <- 0.3*sqrt(4/rchisq(100,df=4))
 y <- matrix(rnorm(100*6,sd=sd),60,10)
 rownames(y) <- paste("Gene",1:60)
@@ -20,15 +19,15 @@ colnames(annotdf) <- c("samples","labels")
 
 designmat <- model.matrix( ~0+  as.factor( annotdf$labels) )  # design matrix
     colnames(designmat) <- levels(as.factor( annotdf$labels))
-
+´`´`++**´``´´´´++´stats::model.matrix
     
 myfit <- lmFit(y, designmat) # fitting the linear model
-    
-contrast.matrix <- makeContrasts(ein-zwei, ein-drei, levels=designmat) ## matrix of contrasts   
-myfit <- contrasts.fit(myfit, contrast.matrix)    
+
+mycontMat <- mymakeContrasts(designmat) # make your matrix of contrasts
+
 myefit <- eBayes(myfit) # moderate standard errors of the estimated log-fold changes
 
-topTable( myefit , coef=1, adjust="BH", number = 10)
+topTable( myefit , coef=1, adjust="BH", number = 20)
 topTable( myefit , coef=2, adjust="BH", number = 10)
 
 # write.table
---
+++ libraries/makeallContrasts.R
@@ -0,0 +1,39 @@
+##################################################################
+### This program receives a design matrix as input and retrieves a contrasts' matrix 
+### with all the pairwise possible contrasts
+### The design matrix should be created like the output of stats::model.matrix
+######  Note: Maybe you can improve it asking for a label called "Control" 
+######  If one of the levels/labels of the design matrix has the name reference/control,
+######  first all the comparisons will be made against it and then the other pairwise comparisons
+######  will follow an alphabetical order.
+##################################################################
+if (!require("gtools")) {
+  install.packages("gtools", ask =FALSE)
+  library("gtools")
+}
+
+mymakeContrasts <- function( design_input ){ 
+  n = length( colnames(design_input) )
+  ncols_contrast_mat = (n * (n-1)) /2  # total number of pairwise comparisons
+  
+  # creating the scaffold of the contrasts' matrix  
+  required_zero_entries_for_contrast_mat <-  rep(0, ncols_contrast_mat * dim(design_input)[2]  )
+  result_Contrasts_matrix <- matrix( required_zero_entries_for_contrast_mat , nrow = dim(design_input)[2] )  
+  rownames(result_Contrasts_matrix) <- colnames(design_input)
+  
+  
+    comb <-combinations(n,2,v=colnames(design_input) ) 
+    colnames( result_Contrasts_matrix ) <- paste0(comb[,1]," - ",comb[,2])
+  
+    for(k in 1:ncols_contrast_mat){
+      jo1<- grep( comb[k,1] , rownames(result_Contrasts_matrix) )
+      result_Contrasts_matrix[jo1,k] <- 1
+      jo_1 <- grep( comb[k,2] , rownames( result_Contrasts_matrix ) )
+      result_Contrasts_matrix[jo_1,k] <- -1
+
+   #if(length(which(colnames(design_input) %in% "Control"))!=0 ){  
+   # print("There is not a label named Control") if / else 
+  }
+  names(attributes( result_Contrasts_matrix )$dimnames) <- c( "Levels" , "Contrasts" )
+  return(result_Contrasts_matrix)
+  } 
